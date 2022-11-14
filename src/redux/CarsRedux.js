7af8144bc850138f4dd08c9b09214cc93c8df3ed
Your Name <you@example.com>
2022-11-14 23:41:42 +0100

    Search type of sport in FindComp

--- src/redux/CarsRedux.js
+++ src/redux/CarsRedux.js
@@ -5,12 +5,14 @@ export const getAllCurrentCars = ({cars, search}) => {
     if(search.searchActive){
         result = result.filter(c=>c.model.toLowerCase().includes(search.keyword.toLowerCase()))
     }
-    
+
     if(search.typeCar.length > 0) {
         if(search.typeCar === "suv"){
             result = result.filter(car => car.carType == "suv")
         } else if (search.typeCar === "sedan"){
             result = result.filter(car => car.carType == "sedan")
+        } else if (search.typeCar === "sport"){
+            result = result.filter(car => car.carType == "sport")
         }
     }
     return result
